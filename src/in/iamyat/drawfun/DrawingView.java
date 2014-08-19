d3ddffa6a611b5687c9f5a8ffcb475588ac87630
yatintaluja <yatintaluja@gmail.com>
2014-08-19 16:29:55 +0530

    create xml

---
+++ src/in/iamyat/drawfun/DrawingView.java
@@ -0,0 +1,17 @@
+package in.iamyat.drawfun;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DrawingView extends View {
+
+	public DrawingView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setupDrawing();
+	}
+
+	private void setupDrawing() {
+
+	}
+}
