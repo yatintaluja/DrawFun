eba025e61f64092240032f4cba47fae369be2cc3
yatintaluja <yatintaluja@gmail.com>
2014-08-19 17:33:25 +0530

    update

--- src/in/iamyat/drawfun/MainActivity.java
+++ src/in/iamyat/drawfun/MainActivity.java
@@ -1,16 +1,42 @@
 package in.iamyat.drawfun;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends ActionBarActivity {
+
+	private DrawingView drawView;
+	private ImageButton currPaint;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		drawView = (DrawingView) findViewById(R.id.drawing);
+		LinearLayout paintLayout = (LinearLayout) findViewById(R.id.paint_colors);
+
+		currPaint = (ImageButton) paintLayout.getChildAt(0);
+		currPaint.setImageDrawable(getResources().getDrawable(
+				R.drawable.paint_pressed));
+
+	}
+
+	public void paintClicked(View view) {
+		if (view != currPaint) {
+			ImageButton imgView = (ImageButton) view;
+			String color = view.getTag().toString();
+			drawView.setColor(color);
+			imgView.setImageDrawable(getResources().getDrawable(
+					R.drawable.paint_pressed));
+			currPaint.setImageDrawable(getResources().getDrawable(
+					R.drawable.paint));
+			currPaint = (ImageButton) view;
+		}
 	}
 
 	@Override
